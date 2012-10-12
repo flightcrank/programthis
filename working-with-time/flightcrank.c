9e8af467e71fe9ca84a42be3c6d2e3316e87a854
flightcrank <ocaumaxim@gmail.com>
2012-10-13 06:11:32 +1000

    reduced ans simplifed code by a large factor

--- working-with-time/flightcrank.c
+++ working-with-time/flightcrank.c
@@ -1,11 +1,11 @@
 
 #include <stdio.h>
 
-#define DAY 13
-#define MONTH 10
-#define YEAR 2012
+#define DAY 11
+#define MONTH 3
+#define YEAR 1982
 
-enum months {JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC};
+int m[] = {31,28,31,30,31,30,31,31,30,31,30,31};
 
 int is_leap(int year) {
 
@@ -49,272 +49,48 @@ int main() {
 	int leap = leap_num(YEAR - 1);
 	int non_leap = (YEAR - 1) - leap;
 	int days = 0;
+	int i;
 
-	//jan
-	days += (YEAR - 1) * 31;
+	//Add all the days total besise the current specified year
+	for (i = 0; i < 12; i ++) {
 	
-	//feb
-	days += (leap * 29) + (non_leap * 28);
+		//if feb
+		if (i == 1) {
+			
+			days += (leap * 29) + (non_leap * 28);
+			
+		} else {
 
-	//mar
-	days += (YEAR - 1) * 31;
+			days += (YEAR - 1) * m[i];
+		}
+	}
 
-	//apr
-	days += (YEAR - 1) * 30;
-
-	//may
-	days += (YEAR - 1) * 31;
-
-	//jun
-	days += (YEAR - 1) * 30;
-
-	//jul
-	days += (YEAR - 1) * 31;
-
-	//aug
-	days += (YEAR - 1) * 31;
-
-	//sep
-	days += (YEAR - 1) * 30;
-
-	//oct
-	days += (YEAR - 1) * 31;
-
-	//nov
-	days += (YEAR - 1) * 30;
-
-	//dec
-	days += (YEAR - 1) * 31;
-
-
-	switch (MONTH - 1) {
+	//Add all the days in the current year besides the current month to the total so far
+	if (MONTH > 1) {
 		
-		case JAN:
-			
-			days += DAY;
-		break;
+		for (i = 0; i < MONTH - 1; i++) {
 		
-		case FEB:
-			
-			days += 31;//jan	
-			days += DAY;
-		break;
-		
-		case MAR:
-			
-			days += 31;//jan	
-			
-			//feb
-			if (is_leap(YEAR)){
+			// if FEB
+			if (i == 1) {
 				
-				days += 29;
-			} else {
-			
-				days += 28;
-			}
-
-			days += DAY;
-		break;
-
-		case APR:
-
-			days += 31;//jan	
-			
-			//feb
-			if (is_leap(YEAR)){
+				if (is_leap(YEAR)) {
+					
+					days += m[i] + 1;
+				} else {
 				
-				days += 29;
+					days += m[i];
+				}
+			
 			} else {
-			
-				days += 28;
+
+				days += m[i];
 			}
-
-			days += 31;//mar	
-			days += DAY;
-		break;
-		
-		case MAY:
-			
-			days += 31;//jan	
-			
-			//feb
-			if (is_leap(YEAR)) {
-				
-				days += 29;
-
-			} else {
-			
-				days += 28;
-			}
-
-			days += 31;//mar	
-			days += 30;//apr	
-			days += DAY;
-		break;
-		
-		case JUN:
-			
-			days += 31;//jan	
-			
-			//feb
-			if (is_leap(YEAR)) {
-				
-				days += 29;
-
-			} else {
-			
-				days += 28;
-			}
-
-			days += 31;//mar	
-			days += 30;//apr	
-			days += 31;//may	
-			days += DAY;
-		break;
-		
-		case JUL:
-			
-			days += 31;//jan	
-			
-			//feb
-			if (is_leap(YEAR)) {
-				
-				days += 29;
-
-			} else {
-			
-				days += 28;
-			}
-
-			days += 31;//mar	
-			days += 30;//apr	
-			days += 31;//may	
-			days += 30;//jun	
-			days += DAY;
-		break;
-		
-		case AUG:
-			
-			days += 31;//jan	
-			
-			//feb
-			if (is_leap(YEAR)) {
-				
-				days += 29;
-
-			} else {
-			
-				days += 28;
-			}
-
-			days += 31;//mar	
-			days += 30;//apr	
-			days += 31;//may	
-			days += 30;//jun	
-			days += 31;//jul	
-			days += DAY;
-		break;
-		
-		case SEP:
-			
-			days += 31;//jan	
-			
-			//feb
-			if (is_leap(YEAR)) {
-				
-				days += 29;
-
-			} else {
-			
-				days += 28;
-			}
-
-			days += 31;//mar	
-			days += 30;//apr	
-			days += 31;//may	
-			days += 30;//jun	
-			days += 31;//jul	
-			days += 31;//aug	
-			days += DAY;
-		break;
-		
-		case OCT:
-			days += 31;//jan	
-			
-			//feb
-			if (is_leap(YEAR)) {
-				
-				days += 29;
-
-			} else {
-			
-				days += 28;
-			}
-
-			days += 31;//mar	
-			days += 30;//apr	
-			days += 31;//may	
-			days += 30;//jun	
-			days += 31;//jul	
-			days += 31;//aug	
-			days += 30;//sep	
-			days += DAY;
-		break;
-		
-		case NOV:
-			days += 31;//jan	
-			
-			//feb
-			if (is_leap(YEAR)) {
-				
-				days += 29;
-
-			} else {
-			
-				days += 28;
-			}
-
-			days += 31;//mar	
-			days += 30;//apr	
-			days += 31;//may	
-			days += 30;//jun	
-			days += 31;//jul	
-			days += 31;//aug	
-			days += 30;//sep	
-			days += 31;//oct	
-			days += DAY;
-
-		break;
-		
-		case DEC:
-			
-			days += 31;//jan	
-			
-			//feb
-			if (is_leap(YEAR)) {
-				
-				days += 29;
-
-			} else {
-			
-				days += 28;
-			}
-
-			days += 31;//mar	
-			days += 30;//apr	
-			days += 31;//may	
-			days += 30;//jun	
-			days += 31;//jul	
-			days += 31;//aug	
-			days += 30;//sep	
-			days += 31;//oct	
-			days += 30;//nov	
-			days += DAY;
-
-		break;
-		
+		}
 	}
 	
+	//Add the days in the currnt month to the total
+	days += DAY;
+
 	int d = days % 7;
 
 	switch (d) {
